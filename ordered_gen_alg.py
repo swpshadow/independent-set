792ed068867aa10bcf0bf04dbafeabfa4554ed09
thdarkshadow <swp7196@utulsa.edu>
2019-12-25 20:00:22 -0600

    added order based chromosome

--- ordered_gen_alg.py
+++ ordered_gen_alg.py
@@ -1,7 +1,8 @@
-from independent_set import DataSet
+from ordered_ind_set import DataSet
 import random
 import sys
 import time
+
 
 def roulette_selection(pool, data_set):
     probs = []
@@ -22,43 +23,59 @@ def tournament_selection(pool, data_set, prob = 0.75):
     assert len(parent_pool) == len(pool)
     return parent_pool
 
-def cross_over(pool, chromosome_size, crossover = None):
-    if crossover is None:
-        crossover = uniform_cross_over #so crossover can be optional
+def cross_over(pool):
     child_pool = []
     c1, c2 = [], []
-    for x in range(0, int(len(pool)/2)):
-        c1, c2 = crossover(pool[random.randint(0, len(pool)-1)], pool[random.randint(0, len(pool)-1)], chromosome_size)
+    for _ in range(0, int(len(pool)/2)):
+        c1, c2 = cross(pool[random.randint(0, len(pool)-1)], pool[random.randint(0, len(pool)-1)])
         child_pool.extend([c1,c2])
     return child_pool
 
-def single_point_crossover(p1, p2, size):
-    rand = random.randint(1, size)
+def cross(p1, p2):
+    rand = random.randint(1, len(p1))
     c1 = p1[:rand]
     c1.extend(p2[rand:])
+
+    in_child = set(c1)
+    contents = set([x for x in range(0, len(c1))])
+    left_out = contents.difference(in_child)
+    for x in left_out:
+        dup = dup_idx(c1)
+        if dup == -1:
+            break
+        c1[dup] = x
+
     c2 = p2[:rand]
     c2.extend(p1[rand:])
+
+    in_child = set(c2)
+    contents = set([x for x in range(0, len(c2))])
+    left_out = contents.difference(in_child)
+    for x in left_out:
+        dup = dup_idx(c2)
+        if dup == -1:
+            print("oop")
+            break
+        c2[dup] = x
     return (c1, c2)
 
-def uniform_cross_over(p1, p2, size):
-    u = [random.choice([0,1]) for _ in p1]
-    c1 = [p1[x] if u[x] == 0 else p2[x] for x in range(0,size)]
-    c2 = [p1[x] if u[x] == 1 else p2[x] for x in range(0,size)]
-    return (c1, c2)
+def dup_idx(chrom):
+    idx = -1
+    for i in range(len(chrom)):
+        for j in range(i + 1, len(chrom)):
+            if chrom[i] == chrom[j]:
+                return random.choice([i, j])
+    return idx
 
-def random_mutation(pool, mutation_rate = 0.05):
-    for d in pool:
+#single swap mutation
+def mutation(pool, mutation_rate):
+    for p in pool:
         if random.random() < mutation_rate:
-            for _ in range(0, random.randint(0, int(len(d) / 10))):
-                i1 = random.randint(0, len(d)-1)
-                d[i1] = (d[i1] + 1) % 2 #flips the bit
-    return pool
-
-def mutation(pool, mutation_rate = .05):
-    for d in pool:
-        if random.random() < mutation_rate:
-            idx = random.randint(0, len(d)-1)
-            d[idx] = (d[idx] + 1) % 2 #flips the bit
+            i1 = random.randint(0,len(p) - 1)
+            i2 = random.randint(0,len(p) - 1)
+            temp = p[i1]
+            p[i1] = p[i2]
+            p[i2] = temp
     return pool
 
 set_size = 50 #####################
@@ -83,6 +100,8 @@ avg_gens = 0
 print(time.time())
 for _ in range(0,5):
     data_set = DataSet(file_name, size = set_size)
+    # print(data_set.fitness([0, 3, 6, 1, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 9, 2, 4, 5, 7, 8, 10, 11, 13,14, 16, 17, 19, 20, 22, 23, 25, 26, 28, 29, 31, 32, 34, 35, 37, 38, 40, 41, 43, 44, 46, 47, 49]))
+
     pool = data_set.random_pool(pool_size)
     best = (pool[0], data_set.fitness(pool[0]))
     iterations = 0
@@ -94,17 +113,15 @@ for _ in range(0,5):
     #runs until max iterations or it hasn't changed in max_not_changed steps
     while time.time() - start_time < 420 and since_change < max_not_changed and best[1] < optimal:
         elites = []
-        for _ in range(0,2): #always just 2 elites. They are pulled out of parent pool and put back into next gen.
+        for _ in range(0,1): #always just 2 elites. They are pulled out of parent pool and put back into next gen.
             b = data_set.best_in_pool(pool)
             elites.append(pool.pop(b[0]))
 
-        parent_pool = tournament_selection(pool, data_set) #roulette_selection(pool, data_set) / tournament_selection(pool, data_set)
-        child_pool = cross_over(parent_pool, set_size, single_point_crossover) #can pass which cross to use. Single or uniform
-        child_pool = mutation(child_pool, mutation_rate = 0.15) #random_mutation(child_pool, , mutation_rate = .05) / def mutation(child_pool, mutation_rate = .05)
-
-        for id in child_pool: #deals with infeasibles
-            data_set.fix_up(id)
+        parent_pool = tournament_selection(pool, data_set) 
+        child_pool = cross_over(parent_pool) 
+        child_pool = mutation(child_pool, mutation_rate = 0.15) 
         pool = child_pool
+
         pool.extend(elites) #readds the elites to the population
         new_best = data_set.best_in_pool(pool)
         if new_best[1] > best[1]: #if found something with better fitness, replace best found so far
---
+++ ordered_ind_set.py
@@ -0,0 +1,60 @@
+import random
+#chromosome with 10 verts ex: [1, 0, 1, 1, 0, 0, 0, 0, 1, 1]
+class DataSet:
+
+    def __init__(self, file_name, size = 10):
+        self.data = []
+        self.file_name = file_name
+        self.size = size
+        self.read_data()
+
+    #creates a pool of randomly fixed up individuals.
+
+    def random_pool(self, pool_size = 10):
+        pool = []
+        for _ in range(0, pool_size):
+            c = [x for x in range(0, self.size)]
+            random.shuffle(c)
+            pool.append(c)
+        return pool
+
+    #calculates the fitness of a chromosome if feasable
+    def fitness(self, chromosome):
+        idx = 1
+        while self.is_feasable(chromosome[:idx]):
+            idx += 1
+        return idx - 1
+
+    #checks to see if chromosome is feasible
+    def is_feasable(self, chromosome):
+        for curr_idx in range(0, len(chromosome)):
+            for prev_vert in chromosome[curr_idx:]:
+                if self.data[chromosome[curr_idx]][prev_vert] == 1:
+                    return False
+        return True
+
+    #sums the fitnesses of all the chromosomes in the pool. used for selection in genetic algs
+    def sum_pool_fitness(self, pool):
+        sum = 0
+        for p in pool:
+            sum += self.fitness(p)
+        return sum
+
+    #finds the index and fitness value of the best fitness in the pool
+    def best_in_pool(self, pool):
+        index = 0
+        fitness = self.fitness(pool[0])
+        for idx in range(1, len(pool)):
+            fit = self.fitness(pool[idx])
+            if fit > fitness:
+                index = idx
+                fitness = fit
+        return (index, fitness)
+
+    #reads in data to 2d list
+    #reads in a data file with this many vertexes
+    def read_data(self):
+        with open("data//independent_set_{}".format(self.file_name), 'r') as file:
+            for line in file:
+                self.data.append([int(x) for x in line.rstrip().split(',')])
+        file.close()
